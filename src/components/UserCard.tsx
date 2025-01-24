fe454a55795b46963a6eb86bb9f35a5903e27c32
Zuber Khan <zuberkhan034@gmail.com>
2025-01-24 16:21:13 +0530

    ➕ Update AdminPage layout and add UserCard component

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,9 @@
+const UserCard = ({type}:{type:string}) => {
+  return (
+    <div className="rounded-2xl odd:bg-customPurple even:bg-customYellow p-4">
+test
+    </div>
+  )
+};
+
+export default UserCard;
