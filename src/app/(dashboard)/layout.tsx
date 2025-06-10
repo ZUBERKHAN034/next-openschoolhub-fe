779a90029b9c65bec167d72dc198063f93d75fc7
Zuber Khan <zuberkhan034@gmail.com>
2025-06-10 23:18:02 +0530

    ✨ Add Announcements component and integrate it into AdminPage layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,7 +14,7 @@ const DashboardLayout = ({
         <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-2">
           <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
             <Image src="/logo.png" alt="logo" width={32} height={32} />
-            <span className="hidden lg:block">Open School Hub</span>
+            <span className="hidden lg:block font-bold">Open School Hub</span>
           </Link>
           <Menu />
         </div>
